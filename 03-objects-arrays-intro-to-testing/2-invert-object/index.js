400cf76148a79c21adef27100afdc7ad96e0b888
Toma Troshkina <toma.trosh@yandex.ru>
2022-08-08 13:19:38 +0300

    add third module tasks

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,8 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (obj) {
+    const entries = Object.entries(obj);
+    return Object.fromEntries(entries.map(item => [...item].reverse()))
+  }
 }
